efe456122aa4b1e6315e241583418e32bcc98942
anuragjatofficial <anurag.1101140@gmail.com>
2023-06-30 20:51:44 +0530

    created navbar using , counter button using react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { MyButton, NavBar } from './nav.js';
+import './nav.js';
+
+function App() {
+  return (
+    <>
+      <div>
+        <NavBar/>
+        <h1>My first react App</h1>
+        <MyButton />
+      </div>
+    </>
+  );
+}
+
+export default App;
