b1155e7fc729870957226b627ff735f8e384d09d
anuragjatofficial <anurag.1101140@gmail.com>
2023-07-01 12:27:00 +0530

    created custom select tag with react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { MyButton, NavBar } from './nav.js';
 import './nav.js';
+import { SelectTag } from './select.js';
 
 function App() {
   return (
     <>
       <div>
         <NavBar/>
+        <SelectTag/>
         <h1>My first react App</h1>
         <MyButton />
       </div>
